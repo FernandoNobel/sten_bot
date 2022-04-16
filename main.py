b501fe03bc58b5bb2432422c9a1bded7b59dd05b
FernandoNobel <fersann1@upv.es>
2022-04-16 20:28:37 +0200

    Add combat_table

--- main.py
+++ main.py
@@ -5,11 +5,70 @@ import random
 from discord.ext import commands
 from dotenv import load_dotenv
 
+from tabulate import tabulate
+
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix=',')
 
+bot.combat_data = []
+
+#bot.combat_data.append({
+#    'id': 1,
+#    'name': 'Lobo',
+#    'ini': 5,
+#    'agu': 6,
+#    'vid': 7,
+#    'est': 8
+#    })
+#
+#bot.combat_data.append({
+#    'id': 2,
+#    'name': 'Guerrero',
+#    'ini': 10,
+#    'agu': 6,
+#    'vid': 7,
+#    'est': 8
+#    })
+#
+#bot.combat_data.append({
+#    'id': 3,
+#    'name': 'Soldado',
+#    'ini': 8,
+#    'agu': 6,
+#    'vid': 7,
+#    'est': 8
+#    })
+
+def print_combat_table():
+    # In this variable we prepare the data to show in the combat table.
+    data = [] 
+
+    # Prepare the data of each character and add it to data.
+    for character in bot.combat_data:
+        pj = []
+
+        pj.append( character['id'] )
+        pj.append( character['nombre'] )
+        pj.append( character['ini'] )
+        pj.append( character['agu'] )
+        pj.append( character['vid'] )
+        pj.append( character['est'] )
+
+        data.append(pj)
+
+    # Sort the characters by initiative.
+    data = sorted(data, key=lambda x: x[2], reverse=True)
+
+    # Prepare the combat_table text to send to discord.
+    result = '**Mesa de combate**\n'
+    result += '```\n' 
+    result += (tabulate(data, headers=["Id", "Nombre", "Ini", "Agu", "Vid", "Est"])) 
+    result += '\n```'
+
+    return result
+ 
 @bot.command(
     name='saludo', 
     help='Manda un saludo por el chat',
@@ -31,5 +90,39 @@ async def roll(ctx, number_of_runes: int):
         i += 1
     
     await ctx.send(f'{ctx.author} tira {number_of_runes} runas obteniendo {result} éxitos.')
+
+
+@bot.command(
+    name='mesa_de_combate', 
+    help='Muestra la mesa de combate.',
+    aliases=['c']
+    )
+async def combat_table(ctx):
+  
+    await ctx.send(print_combat_table())
+
+@bot.command(
+    name='new', 
+    help='Añade un nuevo personaje a la mesa de combate.'
+    )
+async def new_character(ctx):
+    highest_id = 0
+
+    for character in bot.combat_data:
+        if character['id'] > highest_id:
+            highest_id = character['id']
+
+    character =  {
+        'id': highest_id+1,
+        'nombre': 'sin-nombre',
+        'ini': 0,
+        'agu': 0,
+        'vid': 0,
+        'est': 0
+        }
+
+    bot.combat_data.append(character)
+    
+    await ctx.send(print_combat_table())
 
 bot.run(TOKEN)
