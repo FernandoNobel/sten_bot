ae058cc41d29435d10e54185d79531e9e35c1f43
FernandoNobel <fersann1@upv.es>
2022-04-16 20:59:14 +0200

    Add new more options

--- main.py
+++ main.py
@@ -14,33 +14,6 @@ bot = commands.Bot(command_prefix=',')
 
 bot.combat_data = []
 
-#bot.combat_data.append({
-#    'id': 1,
-#    'name': 'Lobo',
-#    'ini': 5,
-#    'agu': 6,
-#    'vid': 7,
-#    'est': 8
-#    })
-#
-#bot.combat_data.append({
-#    'id': 2,
-#    'name': 'Guerrero',
-#    'ini': 10,
-#    'agu': 6,
-#    'vid': 7,
-#    'est': 8
-#    })
-#
-#bot.combat_data.append({
-#    'id': 3,
-#    'name': 'Soldado',
-#    'ini': 8,
-#    'agu': 6,
-#    'vid': 7,
-#    'est': 8
-#    })
-
 def print_combat_table():
     # In this variable we prepare the data to show in the combat table.
     data = [] 
@@ -68,17 +41,49 @@ def print_combat_table():
     result += '\n```'
 
     return result
- 
+
+def generate_new_id():
+    highest_id = 0
+
+    for character in bot.combat_data:
+        if character['id'] > highest_id:
+            highest_id = character['id']
+
+    new_id = highest_id + 1
+
+    return new_id
+
+def modify_character(character_id, parameter_name, parameter_value):
+
+    parameter_name = parameter_name.lower()
+
+    if parameter_name == 'id':
+        return
+
+    if parameter_name == 'nombre':
+        parameter_value = str(parameter_value)
+    else:
+        parameter_value = int(parameter_value)
+
+    i = 0
+    while i < len(bot.combat_data):
+        if (bot.combat_data[i])['id'] == character_id:
+            (bot.combat_data[i])[parameter_name] = parameter_value
+            break
+        i += 1
+
+    return
+
 @bot.command(
-    name='saludo', 
+    name='greetings', 
     help='Manda un saludo por el chat',
-    aliases=['hola','hi','hello']
+    aliases=['g']
     )
 async def greetings(ctx):
     await ctx.send('Saludos, soy sten-bot, que las runas te sean propicias.')
 
 @bot.command(
-    name='runas', 
+    name='roll', 
     help='Lanza X runas.',
     aliases=['r']
     )
@@ -93,26 +98,23 @@ async def roll(ctx, number_of_runes: int):
 
 
 @bot.command(
-    name='mesa_de_combate', 
+    name='show', 
     help='Muestra la mesa de combate.',
-    aliases=['c']
+    aliases=['s']
     )
-async def combat_table(ctx):
+async def show(ctx):
     await ctx.send(print_combat_table())
 
 @bot.command(
     name='new', 
-    help='Añade un nuevo personaje a la mesa de combate.'
+    help='Añade un nuevo personaje a la mesa de combate.',
+    aliases=['n']
     )
-async def new_character(ctx):
-    highest_id = 0
-
-    for character in bot.combat_data:
-        if character['id'] > highest_id:
-            highest_id = character['id']
+async def new(ctx, *args):
+    character_id = generate_new_id()
 
     character =  {
-        'id': highest_id+1,
+        'id': character_id,
         'nombre': 'sin-nombre',
         'ini': 0,
         'agu': 0,
@@ -121,7 +123,13 @@ async def new_character(ctx):
         }
 
     bot.combat_data.append(character)
-    
+
+    i = 0
+
+    while i < len(args):
+        modify_character(character_id, args[i], args[i+1])
+        i += 2
+
     await ctx.send(print_combat_table())
 
 @bot.command(
@@ -134,27 +142,12 @@ async def clean(ctx):
 
 @bot.command(
     name='modify', 
-    help='Modifica un parametro de un personaje'
+    help='Modifica un parametro de un personaje',
+    aliases=['m']
     )
-async def modify_character(ctx, id: int, parameter_name: str, parameter_value):
+async def modify(ctx, character_id: int, parameter_name: str, parameter_value):
 
-    i = 0
-
-    parameter_name = parameter_name.lower()
-
-    if parameter_name == 'id':
-        return
-
-    if parameter_name == 'nombre':
-        parameter_value = str(parameter_value)
-    else:
-        parameter_value = int(parameter_value)
-
-    while i < len(bot.combat_data):
-        if (bot.combat_data[i])['id'] == id:
-            (bot.combat_data[i])[parameter_name] = parameter_value
-            break
-        i += 1
+    modify_character(character_id, parameter_name, parameter_value)
 
     await ctx.send(print_combat_table())
 
